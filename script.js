136f5cde602c96b15ff79550c29a9097b3e839a5
Zeb <73417591+Zeb228228@users.noreply.github.com>
2025-07-27 07:25:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-console.log("Mini App loaded");
-console.log("User info:", Telegram.WebApp.initDataUnsafe.user);
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
